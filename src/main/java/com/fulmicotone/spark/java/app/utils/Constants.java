682e8a8c9ebb7064efd4b2b6117f5f144fbdc269
dino <>
2018-07-19 18:17:17 +0200

    adding some date functions

--- src/main/java/com/fulmicotone/spark/java/app/utils/Constants.java
+++ src/main/java/com/fulmicotone/spark/java/app/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.fulmicotone.spark.java.app.utils;
 
 public class Constants {
-    public final String[] TIME_COLUMNS=new String[]{"year","month","day"};
-    public final String[] DEEP_TIME_COLUMNS=new String[]{"year","month","day","hour"};
+    public static  final String[] TIME_COLUMNS=new String[]{"year","month","day"};
+    public static final String[] DEEP_TIME_COLUMNS=new String[]{"year","month","day","hour"};
 }
